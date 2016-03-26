9903253ff1e31ed2a3eb50a73ccfe91c6fc454cf
Alex Erickson <aelliotterickson@gmail.com>
2016-03-26 16:18:17 -0700

    round out interactivity

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ var map;
          	zoom: 15,
          	zoomControl: true,
     		zoomControlOptions: {
-    			style: google.maps.ZoomControlStyle.LARGE,
         		position: google.maps.ControlPosition.RIGHT_TOP
     		},
         });
@@ -16,24 +15,20 @@ var map;
 $(document).ready(function(){
 
 	var shows = [];
-
 	var listingHTML = '';
-	var location = '';
 
 	function getLocation(){
-		location += $('#city').val();
-		location += ',';
-		location += $('#state').val();
+		return $('#city').val() + ',' + $('#state').val();
 	}
 
 	function getShows(item){
 	
-		// the parameters we need to pass in our request to StackOverflow's API
+		// the parameters we need to pass in our request to Bandsintown's API
 		var request = { 
-			location: location,
+			location: getLocation(),
 			app_id: 'supp',
 			api_version: '2.0',
-			limit: 100
+			limit: 75
 		};
 		
 		$.ajax({
@@ -47,24 +42,37 @@ $(document).ready(function(){
 				shows.push(result[i]);
 			}
 			if (shows.length === 0) {
-				alert('There don\'t seem to be any shows in this location today!')
+				nothingToShow();
 			} else {
-				console.log(shows);
 				compileListings();
 				showListings();
 				mapShows();
+				$('.loading-screen').hide('fast');
+				$('.show-listings').show('fast');
+				$('.map').css('height', '65%');
 			}
 		})
 		.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
-			alert('error!');
+			nothingToShow();
 		});
-
 		return
 	};
 
+	function nothingToShow(){
+		console.log('failure');
+		$('.fail-screen').show();
+		$('.content').on('click', '.fail-screen', function(){
+			$('#city').val('');
+			$('#state').val(''); 
+			$(this).hide('fast');
+			$('.loading-screen').hide();
+			$('.landing').show('fast');
+		});
+	}
+
 	function compileListings() {
 		for (var i = 0; i < shows.length; i ++){
-			listingHTML += "<li><div class='listing' data-id='" + shows[i].id + "'><h3 class='listing-title'>" + shows[i].artists[0].name + " @ " + shows[i].venue.name + "</h3><h4 class='listing-subheading'>" + moment(shows[i].datetime).format( 'MMM DD, YYYY h:mm a' ) + "</h4><div class='otherInfo' id='other" + shows[i].id + "'><h4 class='listing-subheading'><a href='" + shows[i].url + "'>Event Page</a></h4><h4 class='listing-subheading'><a href='" + shows[i].ticket_url + "'>Get Tickets</a></h4></div></div></li>";
+			listingHTML += "<li><div class='listing' data-id='" + shows[i].id + "' data-lat='" + shows[i].venue.latitude + "' data-lng='" + shows[i].venue.longitude + "'><h3 class='listing-title'>" + shows[i].artists[0].name + " @ " + shows[i].venue.name + "</h3><h4 class='listing-subheading'>" + moment(shows[i].datetime).format( 'MMM DD, YYYY h:mm a' ) + "</h4><div class='otherInfo' id='other" + shows[i].id + "'><h4 class='listing-subheading'><a href='" + shows[i].url + "'>Event Page</a></h4><h4 class='listing-subheading'><a href='" + shows[i].ticket_url + "'>Get Tickets</a></h4></div></div></li>";
 		}
 	}
 
@@ -86,7 +94,7 @@ $(document).ready(function(){
 		    bounds.extend(position)
 		   	google.maps.event.addListener(marker, 'click', (function(marker, i) {
 		       	return function() {
-	        		infowindow.setContent(shows[i].artists[0].name);
+	        		infowindow.setContent('<h1>' + shows[i].artists[0].name + '</h1><h2>' + shows[i].venue.name + '</h2><h2>' + moment(shows[i].datetime).format( "MMM DD, YYYY h:mm a" ) + '</h2>');
 		        	infowindow.open(map, marker);
 		        }
 		    	})(marker, i));
@@ -100,17 +108,39 @@ $(document).ready(function(){
 
 	$('.location').on('click', '.submit', function(e){
 		e.preventDefault();
+		$('.landing').hide('fast');
+		$('.loading-screen').show('fast');
 		getLocation();
 		getShows();
-		$('.map').css('height', '65%');
-		$('.landing').hide('fast');
-		$('.show-listings').show('fast');
 	});
 
 	$('.show-listings').on('click', '.listing', function(){
 		$(this).find('.otherInfo').toggle('fast');
+		var thisLat = $(this).data('lat');
+		var thisLng = $(this).data('lng'); 
+		function zoomToShow(){
+			map.setZoom(19);
+			map.panTo(new google.maps.LatLng(thisLat, thisLng));
+		}
+		zoomToShow();
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
